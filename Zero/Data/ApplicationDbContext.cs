2bef38f23cfd41f5075b4119d6976ba77861d1ea
jeong_da_mi <ekal3831@gmail.com>
2023-02-03 15:11:35 +0900

    초기 커밋.

---
+++ Zero/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Zero.Models;
+
+namespace Zero.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
